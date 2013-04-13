a968abfeea2e8b207dd5a10d09d5a9258801c072
atupal <kangleyu@hustunique.com>
2013-04-13 21:56:22 +0800

    daily

--- curui/npyscreen_ui/npyscreen_ts.py
+++ curui/npyscreen_ui/npyscreen_ts.py
@@ -1,15 +1,17 @@
+#coding=utf-8
+
 import npyscreen
 
-class myEmployeeForm(npyscreen.Form):
+class form(npyscreen.Form):
     def create(self):
-        self.myName        = self.add(npyscreen.TitleText, name='Name')
-        self.myDepartment = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=3, name='Department', values = ['Department 1', 'Department 2', 'Department 3'])
-        self.myDate        = self.add(npyscreen.TitleDateCombo, name='Date Employed')
+        self.myName = self.add(npyscreen.TitleText, name='Name')
+        self.myDepartment = self.add(npyscreen.TitleText, name="departement")
+        self.myDate = self.add(npyscreen.TitleDateCombo, name='date')
 
 def myFunction(*args):
-    F = myEmployeeForm(name = "New Employee")
+    F = form(name = "friend list", lines=40,columns = 30)
     F.edit()
-    return "Created record for " + F.myName.value
+    return F.myName.value + 'asdf'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print npyscreen.wrapper_basic(myFunction)
--- curui/urwid_ui/urwid_ts.py
+++ curui/urwid_ui/urwid_ts.py
@@ -1,20 +1,35 @@
 import urwid
 
-palette = [('I say', 'default,bold', 'default', 'bold'),]
-ask = urwid.Edit(('I say', u"What is your name?\n"))
-reply = urwid.Text(u"")
-button = urwid.Button(u'Exit')
-div = urwid.Divider()
-pile = urwid.Pile([ask, div, reply, div, button])
-top = urwid.Filler(pile, valign='top')
+choices = {'friend', 'friend2', 'yukangle', '1', '2', '3'}
+group_list = {
+        'friend'   : {'tom', 'jimi', 'marry'},
+        'friend2'  : {'burenshi', 'nimei'},
+        'yukangle' : {'atupal', 'fy', 'wsq'},
+        '1'        : {'1', '11', '111'},
+        '2'        : {'2', '222', '2222'},
+        '3'        : {'3', '33', '333'}
+        }
 
-def on_ask_change(edit, new_edit_text):
-    reply.set_text(('I say', u"Nice to meet you, %s" % new_edit_text))
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    for c in choices:
+        button = urwid.Button(c)
+        urwid.connect_signal(button, 'click', group_chosen, c)
+        body.append(urwid.AttrMap(button, None, focus_map = 'reversed'))
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
-def on_exit_clicked(button):
+def group_chosen(button, choice):
+    response = urwid.Text(['You are chose', choice, '\n'])
+    done = urwid.Button('ok')
+    urwid.connect_signal(done, 'click', exit_program)
+    main.original_widget = urwid.Filler(urwid.Pile([response, urwid.AttrMap(done, None, focus_map='reserved')]))
+
+def  exit_program(button):
     raise urwid.ExitMainLoop()
 
-urwid.connect_signal(ask, 'change', on_ask_change)
-urwid.connect_signal(button, 'click', on_exit_clicked)
-
-urwid.MainLoop(top, palette).run()
+main = urwid.Padding(menu('friend', choices), left = 2, right = 2)
+top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
+        align = 'left', width = ('relative', 20),
+        valign = 'top', height = ('relative', 90),
+        min_width = 20, min_height = 9)
+urwid.MainLoop(top, palette = [('reversed', 'standout', '')]).run()
