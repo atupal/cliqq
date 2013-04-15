d1005175dfefe2f12c59fe02377b18e44a75d9f2
atupal <kangleyu@hustunique.com>
2013-04-15 19:11:33 +0800

    base

--- curui/urwid_ui/urwid_ts.py
+++ curui/urwid_ui/urwid_ts.py
@@ -16,7 +16,7 @@ class top():
     def __init__(self):
         self.flag = 0
     def line_dialog(self, uid, Text = None):
-        text = urwid.Text(uid, align = 'center')
+        text = urwid.Text(uid, align = 'center', wrap='clip')
         for i in Text:
             text.set_text(text.text + '\n' + i)
         return text
@@ -47,13 +47,14 @@ class top():
             return
         text = self.line_dialog(uid = '123', Text = ['11241234', 'q234'])
         #self.body.append(text)
-        self.main.original_widget = urwid.Columns([urwid.Padding(urwid.ListBox(urwid.SimpleFocusListWalker(self.body)), left = 2, right = 2), text])
+        #self.main.original_widget = urwid.Columns([urwid.Filler(urwid.Padding(urwid.ListBox(urwid.SimpleFocusListWalker(self.body)), left = 2, right = 2)), text])
+        self.main.original_widget = urwid.Filler(urwid.Pile([text, urwid.Divider(), urwid.Edit('content: '), urwid.Button('cancel'), urwid.Button('send')]))
         self.flag = 1
 
     def begin(self):
         self.main = urwid.Padding(self.menu('friend', choices), left = 2, right = 2)
         self.left_bar = urwid.Overlay(self.main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-                align = 'left', width = ('relative', 20),
+                align = 'left', width = ('relative', 40),
                 valign = 'top', height = ('relative', 90),
                 min_width = 20, min_height = 9)
         #left_bar = urwid.Pile([left_bar, open_line])
