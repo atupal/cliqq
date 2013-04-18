43ffc329d820dca09b60c682fd3e2a87dc7d67ef
atupal <kangleyu@hustunique.com>
2013-04-18 22:57:39 +0800

    js filE

--- test/xline.cpp
+++ test/xline.cpp
@@ -1,52 +1,39 @@
 
 #include <X11/Xlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+#define NIL (0)
 
 int main() {
-    Display *dsp = XOpenDisplay(NULL);
+    Display *dsp = XOpenDisplay(NIL);
 
-    if (!dsp) {
-        return 1;
+    int blackColor = BlackPixel(dsp, DefaultScreen(dsp));
+    int whiteColor = WhitePixel(dsp, DefaultScreen(dsp));
+
+    Window win = XCreateSimpleWindow(dsp, DefaultRootWindow(dsp), 0, 0,
+                    200, 100, 0, blackColor, blackColor);
+
+    XSelectInput(dsp, win, StructureNotifyMask);
+
+    XMapWindow(dsp, win);
+
+    GC gc = XCreateGC(dsp, win, 0, NIL);
+
+    XSetForeground(dsp, gc, whiteColor);
+
+    for (;;) {
+        XEvent e;
+        XNextEvent(dsp, &e);
+        if (e.type == MapNotify) 
+            break;
     }
 
-    int screenNumber = DefaultScreen(dsp);
-    unsigned long white = WhitePixel(dsp, screenNumber);
-    unsigned long black = BlackPixel(dsp, screenNumber);
+    XDrawLine(dsp, win, gc, 10, 60, 180, 20);
+    XFlush(dsp);
+    sleep(3);
 
-    Window win = XCreateSimpleWindow(dsp, 
-            DefaultRootWindow(dsp),
-           50, 50,  //origin
-           200, 200, //size
-           0, black, //border
-           white ); //backgd
-
-
-    XMapWindow( dsp, win );
-
-    long eventMask = StructureNotifyMask;
-    XSelectInput( dsp, win, eventMask );
-
-    XEvent evt;
-
-    do {
-        XNextEvent(dsp, &evt);  //calss XFlush()
-    }while (evt.type != MapNotify);
-
-    GC gc = XCreateGC(dsp, win, 0, NULL);
-    XSetForeground(dsp, gc, black);
-
-    XDrawLine(dsp, win, gc, 10, 10, 190, 190);
-
-    eventMask = ButtonPressMask | ButtonReleaseMask;
-    XSelectInput(dsp, win, eventMask);   //override prev
-
-    do {
-        XNextEvent(dsp, &evt); //Calls XFlush()
-    } while (evt.type != ButtonRelease);
-
-    XDestroyWindow(dsp, win);
-    XCloseDisplay(dsp);
-
-        
     return 0;
 }
+
 
